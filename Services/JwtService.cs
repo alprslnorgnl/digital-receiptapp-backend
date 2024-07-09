9c254295cdb9104c3a91f3ed50a2bb7bf0c513d7
idris <alparslanorgnl@gmail.com>
2024-07-09 22:13:41 +0300

    JwtService edited

---
+++ Services/JwtService.cs
@@ -0,0 +1,51 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+public interface IJwtService
+{
+    Task<string> GenerateJwtToken(User user);
+}
+
+public class JwtService : IJwtService
+{
+    private readonly IConfiguration _configuration;
+    private readonly ApplicationDbContext _context;
+
+    public JwtService(IConfiguration configuration, ApplicationDbContext context)
+    {
+        _configuration = configuration;
+        _context = context;
+    }
+
+    public async Task<string> GenerateJwtToken(User user)
+    {
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.UserId.ToString()),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration["Jwt:Issuer"],
+            audience: _configuration["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.Now.AddDays(int.Parse(_configuration["Jwt:ExpireDays"]!)),
+            signingCredentials: creds
+        );
+
+        var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+        user.Token = tokenString;
+        _context.Users.Update(user);
+        await _context.SaveChangesAsync();
+
+        return tokenString;
+    }
+}
